820da4239f6a62b1109d458a46a263917b597e0e
vely44 <vely44@gmail.com>
2019-03-16 10:14:40 +0100

    Saturday 16Mar

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     array<int, 3> ar{2,-2,3};
+    array<int,3> ar2;
 
     for(auto& el:ar)
     {
@@ -13,10 +14,16 @@ int main()
         el*=10;
     }
     cout<<endl;
-    ar[0]=10*ar.at(1);
+    int i = 0;
+    //ar[0]=10*ar.at(1);
     for(const auto el:ar)
     {
-        cout<<el<<"";
+        cout<<el<<" ";
+        ar2.at(i) = el;
+        i++;
     }
-
+    for(const auto el:ar2)
+    {
+        cout<<el<<" ";
+    }
 }
